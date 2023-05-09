3c31d3d532f34cf8f5d11516a19d3834df797753
riteshsaini01 <b210885@skit.ac.in>
2023-05-09 19:05:25 +0530

    Section 2 initial commit

---
+++ 02_section_Datatype-variables/Variables1.java
@@ -0,0 +1,15 @@
+
+
+public class Variables1 {
+
+    public static void main(String[] args) {
+        int x;
+        int roll_number$student;
+        int _x;
+        
+        int rollNumberOfStudent;
+        float averageMarksOfClass;
+        
+    }
+    
+}
