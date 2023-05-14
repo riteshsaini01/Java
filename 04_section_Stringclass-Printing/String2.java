e5743632f5394b62ff5f96bece21e114b9a48873
riteshsaini01 <b210885@skit.ac.in>
2023-05-14 18:17:28 +0530

    Adding string codes

---
+++ 04_section_Stringclass-Printing/String2.java
@@ -0,0 +1,27 @@
+public class String2 {
+    public static void main(String[] args){
+
+        String str=new String("vs code");
+        
+        /*
+        //System.out.println(str.length());
+        int len=str.length();
+        System.out.println(len);*/
+        
+        
+        /*String str2=str.toUpperCase();
+        System.out.println(str2);*/
+        
+        
+        /*str=str.trim();
+        System.out.println(str);*/
+        
+        /*String str2=str.substring(3);
+        System.out.println(str2);*/
+        
+        String str2=str.replace('e', 'M');
+        System.out.println(str2);
+        
+        System.out.println(str2+ " " +str);
+    }
+}
