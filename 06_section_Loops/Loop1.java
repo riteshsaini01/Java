49eec0d2ce9b39bfb0f215990269b9512522ad40
riteshsaini01 <b210885@skit.ac.in>
2023-05-17 21:49:33 +0530

    Adding new branch section_06

---
+++ 06_section_Loops/Loop1.java
@@ -0,0 +1,23 @@
+public class Loop1
+{
+    public static void main(String [] args)
+    {
+
+        int i=100;
+
+        //Do-While loop
+        do{
+            System.out.println(i);
+            i=i*2;
+        }while(i<100);
+
+
+
+
+        //While Loop
+       /*while(i<100){
+            System.out.println(i);
+            i=i*2;
+        }*/
+    }
+}
