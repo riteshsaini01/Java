7e520c36f6f34cea7210b8b711ad0d2a06374927
riteshsaini01 <b210885@skit.ac.in>
2023-06-01 20:49:10 +0530

    section_15 completed

--- 15_section_Packages/Package.java
+++ 15_section_Packages/Package.java
@@ -1,52 +1,11 @@
-class Demo1
-{
-    int a=10;
-    //private.
-    public int b=20;
-    protected int c=30;
-    public int d=40;
-    
-    public void display()
-    {
-        System.out.println(a+b+c+d);
-    }
-}
-/*Public class Demo2()
-{
-    Demo1 d=new Demo1();
-    public void show()
-    {
-        System.out.println(d.a+d.b+d.c+d.d);
-    }
-    
-}*/
-class Demo3 extends Demo1
-{
-    Demo1 d=new Demo1();
-    public void show()
-    {
-        System.out.println(d.a+d.b+d.c+d.d);
-    }
-}
+//import java.lang.String;
+import java.lang.Runnable;
 
-class Demo4 extends Demo1
+class Package
 {
-    public void show()
+    public static void main(String args[])
     {
-        System.out.println(a+b+c+d);
+        java.lang.String str;
+        str=new java.lang.String("Hello");
     }
-}
-    
-
-public class Package {
-
-    
-    public static void main(String[] args) 
-    {
-      Demo1 d1=new Demo1();
-      d1.display();
-      System.out.println(d1.a+d1.b+d1.c+d1.d);
-      
-    }
-    
 }
