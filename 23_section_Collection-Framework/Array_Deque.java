736b562c3f46e0516f3487d661daf9f347f81728
riteshsaini01 <b210885@skit.ac.in>
2023-06-19 10:01:19 +0530

    Adding codes of Priority Queue

--- 23_section_Collection-Framework/Array_Deque.java
+++ 23_section_Collection-Framework/Array_Deque.java
@@ -6,17 +6,19 @@ public class Array_Deque
     {
          ArrayDeque<Integer> dq=new ArrayDeque<>();
          
-         dq.offerLast(10);
+         dq.offerLast(10); //Add last
          dq.offerLast(20);
          dq.offerLast(30);
          dq.offerLast(40);
          
          dq.pollFirst();
          
-         dq.offerLast(1);
-         dq.offerLast(2);
-         dq.offerLast(3);
-         dq.offerLast(4);
+         dq.offerFirst(1); //Add first
+         dq.offerFirst(2);
+         dq.offerFirst(3);
+         dq.offerFirst(4);
+
+         dq.offerLast(21); ;
          
          dq.forEach((x)->System.out.println(x));
          
