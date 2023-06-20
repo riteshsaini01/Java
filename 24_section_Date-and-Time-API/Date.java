3a1b2bb476727706fdfbecc4a132c22a3cfe756d
riteshsaini01 <b210885@skit.ac.in>
2023-06-20 19:18:53 +0530

    Adding new branch section_24

---
+++ 24_section_Date-and-Time-API/Date.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+@SuppressWarnings(value = { "strictfp" })
+strictfp public class Date
+{
+    public static void main(String args[])
+    {
+
+        Date d=new Date();
+        System.out.println(d);
+
+    
+        //System.out.println(System.currentTimeMillis());
+        //System.out.println(Long.MAX_VALUE);
+
+    }
+}
